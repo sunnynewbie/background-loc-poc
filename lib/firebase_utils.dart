0e5b4f79e271e056bb91c07ac19f759f98ea67b8
sunnynewbie <sunny.ptel5497@gmail.com>
2025-01-10 16:08:36 +0530

    collection stucutre udateed.

--- lib/firebase_utils.dart
+++ lib/firebase_utils.dart
@@ -1,15 +1,31 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
-as bg;
+    as bg;
+
 class FirebaseUtil {
   writeLocationToDocument(bg.Location location) async {
-
-    var item ={
-      'lat':location.coords.latitude,
-      'lng':location.coords.longitude,
-      'time':location.timestamp,
-
+    var item = {
+      'lat': location.coords.latitude,
+      'lng': location.coords.longitude,
+      'time': location.timestamp,
     };
-    await FirebaseFirestore.instance.collection("locations").doc(location.uuid).set(item);
+    String id = '';
+
+    if (Platform.isAndroid) {
+      var info = await DeviceInfoPlugin().androidInfo;
+      id = info.id;
+    } else {
+      var info = await DeviceInfoPlugin().iosInfo;
+      id = info.identifierForVendor ?? '';
+    }
+    var doc = FirebaseFirestore.instance
+        .collection("locations")
+        .doc(id)
+        .collection("position");
+    var itemCount = await doc.count().get();
+    await doc.doc("${itemCount.count ?? 0}").set(item);
   }
 }
